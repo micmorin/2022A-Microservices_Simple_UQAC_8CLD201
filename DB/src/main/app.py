e21b6802372a333fa6e93c7a4ec1590000eecd35
Michael Munger <michael.munger1@uqac.ca>
2022-09-25 12:33:21 -0400

    Add database folder and future container

---
+++ DB/src/main/app.py
@@ -0,0 +1,24 @@
+# APPLICATION FACTORY
+
+from flask import Flask
+from main.app_init import apparence, database, login
+from main.blueprints.bp_default import bp_default
+from main.blueprints.bp_users import bp_users
+from main.blueprints.bp_objects import bp_objects
+from main.blueprints.bp_admin import bp_admin
+from main.blueprints.bp_API import bp_api
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.DevConfig')
+    apparence.init_app(app)
+    database.init_app(app)
+    login.init_app(app)
+
+    app.register_blueprint(bp_default)
+    app.register_blueprint(bp_objects)
+    app.register_blueprint(bp_users)
+    app.register_blueprint(bp_admin)
+    app.register_blueprint(bp_api)
+
+    return app
