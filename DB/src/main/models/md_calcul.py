88f0c07064235b53ee1506c3634652daabc86411
Michael Munger <michael.munger1@uqac.ca>
2022-09-25 13:34:25 -0400

    Add calcul table

--- DB/src/main/models/md_calcul.py
+++ DB/src/main/models/md_calcul.py
@@ -2,24 +2,22 @@ from main.app_init.database import db
 from main.models.md_user import User
 from datetime import datetime
 
-class Object(db.Model):
+class Calcul(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Text, nullable=False)
-    setup_date = db.Column(db.Date, nullable=False)
-    upload_date = db.Column(db.Date, nullable=False)
-    status = db.Column(db.Boolean, nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    result = db.Column(db.Integer)
+    date = db.Column(db.Date, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     
     user = db.relationship('User', backref=db.backref('users', lazy=True))
 
     def __repr__(self):
-        return '<Object %r>' % self.name
+        return '<Calcul %r>' % self.name
 
     def to_json(self):
         user = User.query.get_or_404(self.user_id)
         return {"id":self.id,
-                "name":self.name,
-                "setup_date":self.setup_date,
-                "upload_date":self.upload_date,
-                "status":self.status,
+                "body":self.body,
+                "result":self.result,
+                "date":self.date,
                 "user":user.to_json()}
