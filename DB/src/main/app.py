edb657b0c5134efc5c0e2e9359c5539ccd58f69e
Michael Munger <michael.munger1@uqac.ca>
2022-09-25 12:40:32 -0400

    Slim down DB

--- DB/src/main/app.py
+++ DB/src/main/app.py
@@ -1,7 +1,7 @@
 # APPLICATION FACTORY
 
 from flask import Flask
-from main.app_init import apparence, database, login
+from main.app_init import database
 from main.blueprints.bp_default import bp_default
 from main.blueprints.bp_users import bp_users
 from main.blueprints.bp_objects import bp_objects
@@ -11,9 +11,7 @@ from main.blueprints.bp_API import bp_api
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.DevConfig')
-    apparence.init_app(app)
     database.init_app(app)
-    login.init_app(app)
 
     app.register_blueprint(bp_default)
     app.register_blueprint(bp_objects)
