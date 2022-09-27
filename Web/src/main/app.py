4dc2ab6eee569a9d87dffb7fbfdf7690e8e6c86d
Michael Munger <michael.munger1@uqac.ca>
2022-09-27 13:22:32 -0400

    Change objects to calculs in Web

--- Web/src/main/app.py
+++ Web/src/main/app.py
@@ -4,9 +4,8 @@ from flask import Flask
 from main.app_init import apparence, database, login
 from main.blueprints.bp_default import bp_default
 from main.blueprints.bp_users import bp_users
-from main.blueprints.bp_objects import bp_objects
+from main.blueprints.bp_calculs import bp_calculs
 from main.blueprints.bp_admin import bp_admin
-from main.blueprints.bp_API import bp_api
 
 def create_app():
     app = Flask(__name__)
@@ -16,9 +15,8 @@ def create_app():
     login.init_app(app)
 
     app.register_blueprint(bp_default)
-    app.register_blueprint(bp_objects)
+    app.register_blueprint(bp_calculs)
     app.register_blueprint(bp_users)
     app.register_blueprint(bp_admin)
-    app.register_blueprint(bp_api)
 
     return app
