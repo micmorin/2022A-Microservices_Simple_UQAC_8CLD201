badee1f80484c65442955af56e1aebb19e355c60
Jeremy Le Toullec <jeremyletoullec@gmail.com>
2022-11-23 18:53:31 -0500

    Enlever les 0 inutiles, gestion des erreurs, json

--- Complexe/src/run.py
+++ Complexe/src/run.py
@@ -2,14 +2,24 @@ from flask import Flask, request, jsonify
 import math
 
 def calcul(calcul):
-    #Fonction qui calcule le résultat d'un calcul simple
-    if calcul.find("²") != -1:
-        print(calcul)
-        calcul = puissance(calcul)
-        print(calcul)
-    if calcul.find("√") != -1:
-        calcul = racine(calcul)
-    result = eval(str(calcul))
+    try:
+        if calcul.find("²") != -1:
+            calcul = puissance(calcul)
+        if calcul.find("√") != -1:
+            calcul = racine(calcul)
+        result = eval(str(calcul))
+
+        point = str(result).find(".")
+        result = str(result)
+        if point != -1:
+            if result[point+1] == "0" :
+                if ((point+1) == (len(result)-1)):
+                    try:
+                        result = result[:point]
+                    except:
+                        pass
+    except:
+        result = "Erreur"
     return result
 
 def puissance(calcul):
@@ -25,15 +35,10 @@ if __name__ == "__main__":
 
     @app.route("/", methods=['GET'])
     def index():
-        print("index")
         #data = request.get_json()
-        data = {"calc": "√(4*5²)²*√(9)"}
-
+        data = {"calc": "√(4*5²)²*√(10)"}
         str_cal = data["calc"]
-        
         resultat = calcul(str_cal)
-
         return jsonify({"result":resultat})
-
 
     app.run(host='0.0.0.0', port=5000)
--- Simple/calculs_simple.py
+++ Simple/calculs_simple.py
@@ -1,9 +1,21 @@
+from flask import Flask, request, jsonify
+
 def calcul(calcul):
-    #Fonction qui calcule le résultat d'un calcul simple
-    result = eval(str(calcul))
+    try:
+        result = eval(str(calcul))
+    except:
+        result = "Erreur"
     return result
 
-# demande à l'utilisateur de saisir un calcul et affiche le résultat
 if __name__ == "__main__":
-    enter = input("Entrez un calcul : ")
-    print(calcul(enter))
+    app = Flask(__name__)
+
+    @app.route("/", methods=['GET'])
+    def index():
+        #data = request.get_json()
+        data = {"calc": "8/0"}
+        str_cal = data["calc"]
+        resultat = calcul(str_cal)
+        return jsonify({"result":resultat})
+
+    app.run(host='0.0.0.0', port=5000)
