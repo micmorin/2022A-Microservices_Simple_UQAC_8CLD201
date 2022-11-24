0f0248b9ed064cb78dd053c948eadacd274632b1
Jeremy Le Toullec <jeremyletoullec@gmail.com>
2022-11-23 22:02:19 -0500

    Implementation Lambda calculs_simple

--- Simple/calculs_simple.py
+++ Simple/calculs_simple.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+import json
 
 def calcul(calcul):
     try:
@@ -7,15 +7,8 @@ def calcul(calcul):
         result = "Erreur"
     return result
 
-if __name__ == "__main__":
-    app = Flask(__name__)
+def lambda_handler(event, context):
 
-    @app.route("/", methods=['GET'])
-    def index():
-        #data = request.get_json()
-        data = {"calc": "8/0"}
-        str_cal = data["calc"]
-        resultat = calcul(str_cal)
-        return jsonify({"result":resultat})
-
-    app.run(host='0.0.0.0', port=5000)
+    str_cal = event["calc"]
+    resultat = calcul(str_cal)
+    return json.dumps({"result":resultat})
