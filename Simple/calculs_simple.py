335f36e6db3c355d89343dd28fe9a3e30cf85529
Michael Munger <michael.munger1@uqac.ca>
2022-11-27 12:11:26 -0500

    Add connection to Calc Simple

--- Simple/calculs_simple.py
+++ Simple/calculs_simple.py
@@ -1,14 +1,43 @@
 import json
+import requests
 
 def calcul(calcul):
     try:
-        result = eval(str(calcul))
+        resultat = str(eval(str(calcul)))
     except:
-        result = "Erreur"
-    return result
+        resultat = ""
+    return resultat
+    
+def store(userID, calc):
+    try:
+        resultat = calcul(calc)
+        if resultat != "":
+            
+            #### USE REQUESTS to send to DB and Act on response code HERE #####
+            
+            
+            body = "{\"message\":\"OK\", \"result\":"+resultat+"}"
+            status = 200
+        else :
+            body = "{\"message\":\"Calculation failed\", \"result\":\"\"}"
+            status = 510
+    except:
+        body = "{\"message\":\"Storing failed\", \"result\":\"\"}"
+        status = 520
+    
+    return [body, status]
+    
+def returnBuilder(body, status):
+    return {
+        "isBase64Encoded": 'true',
+        "statusCode": status,
+        "headers": {"content-type":"application/json"},
+        "body": body}
+    
 
 def lambda_handler(event, context):
-
-    str_cal = event["calc"]
-    resultat = calcul(str_cal)
-    return json.dumps({"result":resultat})
+    response = store(
+        json.loads(event["body"])["userID"], 
+        json.loads(event["body"])["calc"]
+        )
+    return returnBuilder(response[0], response[1])
