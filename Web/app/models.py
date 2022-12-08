cc911c5bdac98cc005b994bf6f20f5333cccaf01
Elliissa01 <isa.herode@gmail.com>
2022-12-08 02:08:23 -0500

    Retrait "Se souvenir de moi".

--- Web/app/models.py
+++ Web/app/models.py
@@ -5,7 +5,6 @@ from wtforms.validators import DataRequired
 class LoginForm(FlaskForm):
     username = StringField("username", validators=[DataRequired()])
     password = PasswordField("password", validators=[DataRequired()])
-    remember_me = BooleanField("remember_me")
     submit = SubmitField('Submit')
 
 class RegisterForm(FlaskForm):
